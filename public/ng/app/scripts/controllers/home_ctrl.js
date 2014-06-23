7f0cf76ba50c2aa7583d555428d32568d23a5d7d
Matt Emmons <themattemmons@gmail.com>
2014-06-23 16:24:05 -0400

    Fixed bug with sideMenu

--- public/ng/app/scripts/controllers/home_ctrl.js
+++ public/ng/app/scripts/controllers/home_ctrl.js
@@ -1,11 +1,12 @@
 (function() {
   'use strict';
-  this.HomeCtrl = this.controllerModule.controller('HomeCtrl', function($scope, $state, UserService, userScore) {
+  this.HomeCtrl = this.controllerModule.controller('HomeCtrl', function($scope, $state, $rootScope, UserService, userScore) {
     $scope.currentUser = UserService.currentUser();
     $scope.userScore = userScore;
-    return $scope.rankBadge = userScore ? "images/achievements/" + userScore.rank + ".png" : "images/achievements/rookie.png";
+    $scope.rankBadge = userScore ? "images/achievements/" + userScore.rank + ".png" : "images/achievements/rookie.png";
+    return $rootScope.sideMenuEnabled = true;
   });
 
-  this.HomeCtrl.$inject = ['$scope', '$state', 'UserService', 'userScore'];
+  this.HomeCtrl.$inject = ['$scope', '$state', '$rootScope', 'UserService', 'userScore'];
 
 }).call(this);
