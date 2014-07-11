a26e9fb953c97b84d728866346bf4dd93bf3bf8a
Matt Emmons <themattemmons@gmail.com>
2014-07-11 18:31:31 -0400

    radial progress bar-ing

--- public/ng/app/coffee/app.coffee
+++ public/ng/app/coffee/app.coffee
@@ -11,7 +11,8 @@
   'ngResource',
   'underscore',
   'angularMoment',
-  'googlechart'
+  'googlechart',
+  'angular-svg-round-progress'
 ]
 
 @controllerModule = angular.module 'painSquad.controllers', []
--- public/ng/app/scripts/app.js
+++ public/ng/app/scripts/app.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  this.painSquad = angular.module('painSquad', ['ionic', 'painSquad.controllers', 'painSquad.directives', 'painSquad.resources', 'painSquad.services', 'painSquad.filters', 'painSquad.config', 'ngResource', 'underscore', 'angularMoment', 'googlechart']);
+  this.painSquad = angular.module('painSquad', ['ionic', 'painSquad.controllers', 'painSquad.directives', 'painSquad.resources', 'painSquad.services', 'painSquad.filters', 'painSquad.config', 'ngResource', 'underscore', 'angularMoment', 'googlechart', 'angular-svg-round-progress']);
 
   this.controllerModule = angular.module('painSquad.controllers', []);
 
