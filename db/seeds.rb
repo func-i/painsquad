ba95fccc1558c566fdc938e29f89abd43baf0036
Matt Emmons <themattemmons@gmail.com>
2014-07-15 15:04:54 -0400

    Added survey and advice seed files to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 User.destroy_all
 User.create! username: 'test user', email: 'test@uhn.com', password: 'password', healthcare_provider_email: 'test@uhn.com'
 User.create! username: 'matt emmons', email: 'mattwhat@live.com', password: 'password', healthcare_provider_email: 'mattwhat@live.com'
+
+Rake::Task['survey:all'].invoke
+Rake::Task['advice:all'].invoke
