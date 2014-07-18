4b95a8ccd0707b75a4f840f6a96e2f79767d335a
Matt Emmons <themattemmons@gmail.com>
2014-07-18 19:02:21 -0400

    latest build compiled

--- config/initializers/worker.rb
+++ config/initializers/worker.rb
@@ -1,6 +1,5 @@
 # starts the recurring DJ task that runs every 5 minutes
 # checks for user notifications
-
 if ActiveRecord::Base.connection.table_exists? 'delayed_jobs'
   Workers::Notifications.schedule!
 end
---
+++ public/scripts/resources/setting_resource.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+  this.SettingResource = this.resourceModule.factory('SettingResource', function($http, $resource, CONFIG) {
+    $http.defaults.useXDomain = true;
+    return $resource("" + CONFIG.apiUrl + "/settings/", {}, {
+      query: {
+        method: 'GET',
+        isArray: false
+      },
+      update: {
+        method: 'PUT'
+      }
+    });
+  });
+
+  this.SettingResource.$inject = ['$http', '$resource', 'CONFIG'];
+
+}).call(this);
