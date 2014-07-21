68ab431c627ebf911c90f17fcd8497326199e5f2
Matt Emmons <themattemmons@gmail.com>
2014-07-21 09:15:24 -0400

    merging changes from heroku, removing built assets

--- public/scripts/controllers/surveys/boolean_ctrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  this.BooleanCtrl = this.controllerModule.controller("BooleanCtrl", function($scope, $state) {
-    return console.log('boolean ctrl');
-  });
-
-  this.BooleanCtrl.$inject = ['$scope', '$state'];
-
-}).call(this);
--- public/scripts/resources/activity_resource.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  this.ActivityResource = this.resourceModule.factory('Activity', function($http, $resource, CONFIG) {
-    $http.defaults.useXDomain = true;
-    return $resource("" + CONFIG.apiUrl + "/activity", {});
-  });
-
-  this.ActivityResource.$inject = ['$http', '$resource', 'CONFIG'];
-
-}).call(this);
