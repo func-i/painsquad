1c1fd4bc829423582acd779206575212fee0e558
Matt Emmons <themattemmons@gmail.com>
2014-07-22 11:52:06 -0400

    bodymap thumbnails

--- app/services/builder/survey_builder.rb
+++ app/services/builder/survey_builder.rb
@@ -59,6 +59,7 @@ module Builder
     end
 
     def build_test_survey
+      Questions::Other.bodymap(@survey)
       Questions::Checklist.medication_efficacy_slider(@survey)
       Questions::Other.textbox(@survey)
     end
