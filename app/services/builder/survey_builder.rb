e184df8d73f712cf621e877fb198dfd08b522670
Matt Emmons <themattemmons@gmail.com>
2014-07-29 18:03:29 -0400

    more pattern fixes

--- app/services/builder/survey_builder.rb
+++ app/services/builder/survey_builder.rb
@@ -61,6 +61,8 @@ module Builder
     def build_test_survey
       Questions::Other.boolean_has_pain(@survey)
       Questions::Checklist.pain_cause(@survey)
+      Questions::Checklist.medication_efficacy_slider(@survey)
+      Questions::Other.bodymap(@survey)
       Questions::Other.textbox(@survey)
     end
 
